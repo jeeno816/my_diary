c0950279746ef2ee79d36af6c262bd42263991a4
jien040708 <jien0478@kaist.ac.kr>
2025-07-29 00:27:27 +0900

    feat: add backend

---
+++ backend/db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+import os
+
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
+    )
---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from db import get_db
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT 1")
+    return {"status": "DB 연결 성공!"}
