6215bc3938f3e806e71ed169dd1907d156efb8b0
jien040708 <jien0478@kaist.ac.kr>
2025-07-28 14:46:19 +0900

    fix: 로그 추가

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,15 @@
 # db.py
 # 도메인 수정
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+if os.getenv("RAILWAY_ENVIRONMENT"):  # 배포 환경에서만 존재하는 Railway env
+    load_dotenv(".env.production")
+else:
+    load_dotenv() 
+
+print("📦 ENV HOST:", os.getenv("DB_HOST"))
 
 db = mysql.connector.connect(
     host="mydiary-main.up.railway.app",
