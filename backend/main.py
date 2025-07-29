32f1c439b7f59b706f4e590bda9b776fc59d6b6d
jien040708 <jien0478@kaist.ac.kr>
2025-07-29 17:32:46 +0900

    feat: add routers

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from backend.db import get_db
-from routes.diary_routes import router as diary_router
+from backend.routes.diary_routes import router as diary_router
+from backend.routes.photo_routes import router as photo_router
+from backend.routes.person_routes import router as person_router
+from backend.routes.ai_routes import router as ai_router
 
 app = FastAPI()
 
@@ -14,3 +17,6 @@ def root():
 
 # 라우터 등록
 app.include_router(diary_router)
+app.include_router(photo_router)
+app.include_router(person_router)
+app.include_router(ai_router)
